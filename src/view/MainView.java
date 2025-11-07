f7fbcdffc1801ecba281ac69b82fb3c711f3b1ec
NoBirds@Desktop <wuqiufeng1417469@gmail.com>
2025-11-07 16:00:17 +0800

    Switches from colored background to colored characters for better layout.

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -1,9 +1,7 @@
 package view;
 
-import model.Cell;
-import model.CellType;
-import model.ChessBoard;
-import model.Team;
+import model.*;
+import model.chess.*;
 
 import java.io.PrintStream;
 
@@ -12,6 +10,8 @@ public final class MainView {
     public static final String emptyName = "　　　";
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
     public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
     public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
@@ -38,33 +38,34 @@ public final class MainView {
             for (short c = startC; checkIndex(c, false, isRedTurn); c+=increment) {
                 out.print(ANSI_RESET + '|');
                 final CellType cellType = cells[r][c].getType();
-                final String color = switch (cellType) {
+
+                /* background seems not suitable
+                String backgroundColor = switch (cellType) {
                     case TRAP -> ANSI_YELLOW_BACKGROUND;
                     case RIVER -> ANSI_BLUE_BACKGROUND;
                     case DEN -> ANSI_RED_BACKGROUND;
                     case null, default -> ANSI_RESET;
                 };
 
-                final char cellTypeChar = switch (cellType) {
-                    case TRAP -> 'Ｔ';
-                    case RIVER -> '～';
-                    case DEN -> '０';
-                    case null, default -> fullWidthSpace;
+                backgroundColor = "";
+                */
+
+                final String cellTypeChar = switch (cellType) {
+                    case TRAP -> ANSI_YELLOW + "Ｔ";
+                    case RIVER -> ANSI_BLUE + "～";
+                    case DEN -> ANSI_RED + "０";
+                    case null, default -> String.valueOf(fullWidthSpace);
                 };
 
-                out.print(color);
-                if (cells[r][c].getPiece() == null) {
-                    final String outSpace  =
-                            String.valueOf(cellTypeChar) + fullWidthSpace + cellTypeChar;
+                out.print(/* backgroundColor + */cellTypeChar);
 
-                    out.print(outSpace);
-                } else {
-                    out.print(cellTypeChar);
-                    if (cells[r][c].getPiece().team() == Team.RED)
-                        out.print(ANSI_RED);
-                    out.print(cells[r][c].getPiece().getName());
-                    out.print(cellTypeChar);
-                }
+                final Piece piece = cells[r][c].getPiece();
+                if (piece != null) {
+                    final String teamColor = piece.team() == Team.RED ? ANSI_RED : "";
+                    out.print(ANSI_RESET +/* backgroundColor + */ teamColor + piece.getName() + ANSI_RESET);
+                } else out.print(cellTypeChar);
+
+                out.print(/*backgroundColor + */cellTypeChar);
             }
             out.println(ANSI_RESET + '|');
         }
@@ -101,46 +102,3 @@ public final class MainView {
         return instance;
     }
 }
-// Color deprecated
-//public void displayBoard(ChessBoard chessBoard) {
-//    Cell[][] cells = chessBoard.getBoard();
-//    for (int r = 0; r < ChessBoard.rows; r++) {
-//        for (int c = 0; c < ChessBoard.cols; c++) {
-//            out.print("+－－－");
-//        }
-//        out.println('+');
-//        for (int c = 0; c < ChessBoard.cols; c++) {
-//            out.print(ANSI_RESET + "|");
-//
-//            switch (cells[r][c].getType()) {
-//                case EMPTY:
-//                    out.print(ANSI_BLACK_BACKGROUND);
-//                    break;
-//                case TRAP:
-//                    out.print(ANSI_RED_BACKGROUND);
-//                    break;
-//                case RIVER:
-//                    out.print(ANSI_BLUE_BACKGROUND);
-//                    break;
-//                case DEN:
-//                    out.print(ANSI_YELLOW_BACKGROUND);
-//                    break;
-//                case null, default:
-//                    out.print(ANSI_GREEN_BACKGROUND);
-//            }
-//            if(cells[r][c].getPiece() == null)
-//                out.print(emptyName);
-//            else{
-//                out.print("　");
-//                out.print(cells[r][c].getPiece().getName());
-//                out.print("　");
-//            }
-//        }
-//        out.println(ANSI_RESET + '|');
-//
-//    }
-//    for (int c = 0; c < ChessBoard.cols; c++) {
-//        out.print("+－－－");
-//    }
-//    out.println('+');
-//}
