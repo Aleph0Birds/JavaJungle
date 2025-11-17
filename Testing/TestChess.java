2a61838987b614154dbe5fbd98c97a21b43cb501
NoBirds@Desktop <wuqiufeng1417469@gmail.com>
2025-11-17 19:01:19 +0800

    Testing example I guess.

--- Testing/TestChess.java
+++ Testing/TestChess.java
@@ -17,80 +17,8 @@ public class TestChess {
 
     @Test
     public void testMove(){
-        MainModel model = new MainModel();
-        MainView view =  new  MainView();
-        MainController controller = new MainController(model, view);
 
-        controller.initialize();
-        final ChessBoard chessBoard = model.chessBoard;
-
-        Cell redLion = chessBoard.getCell(0, 6);
-        chessBoard.getCell(2, 1).setPiece(redLion.getPiece());
-        redLion.setPiece(null);
-
-        model.gameState = GameState.NotStarted;
-
-        InputStream prevIn = System.in;
-        System.setIn(new ByteArrayInputStream(
-                ("""
-                    2
-                    undo
-                    3 b3 up
-                    3 f8 up
-                    3 g3 left
-                    3 B8 left
-                    3 e3 down
-                    3 c7 left
-                    undo
-                    undo
-                    undo
-                    undo
-                    undo
-                    undo
-                    undo
-                    3 b3 up
-                    3 a7 up
-                    undo
-                    3 g3 up
-                    undo
-                    """)
-                        .getBytes()));
-//        System.setIn(prevIn);
-
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            Command[] choices = CommandList.getCommands(model.gameState);
-            view.displayActionChoices(model.gameState, choices);
-            String input = scanner.nextLine();
-            System.out.println(input);
-            String[] inputs = input.strip().split(" ");
-            if (inputs.length == 0 || inputs[0].isEmpty()) continue;
-
-            final Command targetCmd = parseInput(inputs[0], choices);
-            if (targetCmd != null)
-                targetCmd.invoke(inputs);
-            else
-                view.printErr("Unknown command: " + inputs[0]);
-            if (!scanner.hasNextLine()) {
-                System.setIn(prevIn);
-            }
-        }
     }
 
-    private Command parseInput(String input, Command[] commands) {
-        try {
-            final int num = Integer.parseInt(input) - 1;
-            if (num < 0 || num > commands.length)
-                throw new Exception();
-            return commands[num];
-        } catch (Exception e) {
-            for  (Command command : commands) {
-                if (command.getKey().equals(input.strip()))
-                    return command;
-            }
-        }
 
-        return null;
-    }
 }
--- Testing/TestMain.java
+++ Testing/TestMain.java
@@ -1,3 +1,92 @@
-public class TestMain {
+import controller.MainController;
+import model.GameState;
+import model.MainModel;
+import model.chess.Cell;
+import model.chess.ChessBoard;
+import model.gameIO.Command;
+import model.gameIO.CommandList;
+import org.junit.Test;
+import view.MainView;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class TestMain {
+    @Test
+    public void testMainLoop() {
+        MainModel model = new MainModel();
+        MainView view =  new  MainView();
+
+
+
+        InputStream prevIn = System.in;
+        System.setIn(new ByteArrayInputStream(
+                ("0\nasd\nstart\nend\n1")
+                        .getBytes()));
+
+        MainController controller = new MainController(model, view);
+
+        controller.initialize();
+        controller.startLoop();
+//        System.setIn(prevIn);
+    }
+
+    @Test
+    public void testMainCommand() {
+        MainModel model = new MainModel();
+        MainView view =  new MainView();
+        MainController controller = new MainController(model, view);
+        controller.initialize();
+
+        //name
+        namePlayerTest(controller);
+        //start
+        controller.acceptCommand(CommandList.start, "start");
+        //end
+        controller.acceptCommand(CommandList.end, "end");
+    }
+
+    @Test
+    public void testMainExit() {
+        MainModel model = new MainModel();
+        MainView view =  new MainView();
+        MainController controller = new MainController(model, view);
+        controller.initialize();
+
+        controller.acceptCommand(CommandList.exit, "exit");
+    }
+
+    private static void namePlayerTest(MainController controller) {
+        final Command command = CommandList.namePlayer;
+        String[] args = {"name", };
+        controller.acceptCommand(command, args);
+        args = new String[]{"name", "red",};
+        controller.acceptCommand(command, args);
+        args = new String[]{"name", "red", "Villain"};
+        controller.acceptCommand(command, args);
+        args[1] = "black";
+        controller.acceptCommand(command, args);
+        args[1] = "red";
+        args[2] = "哈基米#1f1e33";
+        controller.acceptCommand(command, args);
+        args[1] = "asd";
+        controller.acceptCommand(command, args);
+    }
+
+    private Command parseInput(String input, Command[] commands) {
+        try {
+            final int num = Integer.parseInt(input) - 1;
+            if (num < 0 || num > commands.length)
+                throw new Exception();
+            return commands[num];
+        } catch (Exception e) {
+            for  (Command command : commands) {
+                if (command.getKey().equals(input.strip()))
+                    return command;
+            }
+        }
+
+        return null;
+    }
 }
